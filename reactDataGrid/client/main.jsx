902c92c94050a5ac7cca41e63c5adf61c5ed7311
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2017-01-29 14:55:12 +0000

    require required, web pack.config superfluous?

--- reactDataGrid/client/main.jsx
+++ reactDataGrid/client/main.jsx
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
  
 import App from '../imports/ui/App.jsx';
+
+require('react-datagrid/index.css') // Essential to importing the CSS from react-datagrid!
  
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
